939622326e03bdbe0716930305a018d0e3dec0ea
TELIDAS <adilet732@gmail.com>
2022-07-03 20:40:52 +0600

    set webhook and replaced worker to web

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from config import bot, dp
+from decouple import config
+
+from config import bot, dp, URL
 from aiogram.utils import executor
 from handlers import client, callback_quiz, extra, admin, notification, inline
 from database import bot_db
 import asyncio
-
 
 """"
 heroku ps:scale worker=1
@@ -12,10 +13,16 @@ heroku ps:scale worker=0
 heroku logs --tail --app pythongeekbot18
 """
 
+
 async def on_startup(_):
+    await bot.set_webhook(URL)
     bot_db.sql_create()
     print("Bot is online")
     asyncio.create_task(notification.scheduler())
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
 
 client.register_handlers_client(dp)
@@ -25,7 +32,16 @@ inline.register_handlers_inline(dp)
 extra.register_handlers_extra(dp)
 
 if __name__ == "__main__":
-    executor.start_polling(dp,
-                           skip_updates=True,
-                           on_startup=on_startup
-                           )
+    # executor.start_polling(dp,
+    #                        skip_updates=True,
+    #                        on_startup=on_startup
+    #                        )
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path="",
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=int(config("PORT", default=5000))
+    )
